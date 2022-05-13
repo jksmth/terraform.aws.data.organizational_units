fe3b9d2a3b26c88bf35bacbe61506fb70c898528
Jake Smith <jakemgsmith@gmail.com>
2022-05-13 13:59:29 +0100

    add example

---
+++ examples/main.tf
@@ -0,0 +1,20 @@
+
+locals {
+  ou_path = "POC/Workloads/Development"
+}
+
+module "orgs" {
+  source = "../"
+}
+
+output "all" {
+  value = module.orgs.organizational_units 
+}
+
+output "development_ou_id" {
+  value = try(module.orgs.organizational_units[local.ou_path].id, "")
+}
+
+provider "aws" {
+  region = "eu-west-2" 
+}
